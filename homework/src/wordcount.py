1d39e8d54441be7a9006ebf66e088a0fb911edb0
Jcastanour <jcastanour@unal.edu.co>
2025-12-06 09:34:46 -0500

    Python3 problem

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,23 +1,22 @@
-## Wordcount
-
-# Ejemplo para casos de uso
-# python3 -m homework data/input data/output
-
-
+# Ejemplo paea casos de uso
+# python -m homework data/input data/output
 import argparse
 
+from homework.src._internals.count_words import count_words
+from homework.src._internals.preprocess_lines import preprocess_lines
 from homework.src._internals.read_all_lines import read_all_lines
+from homework.src._internals.split_into_words import split_lines_into_words
+from homework.src._internals.write_word_counts import write_word_counts
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description="Count Word in files.")
+
+    parser = argparse.ArgumentParser(description="Count word in files.")
     parser.add_argument(
-        "input", type=str, help="Path to the input folder containing files to process."
+        "input", help="Path to the input folder conttaining files to process", type=str
     )
     parser.add_argument(
-        "output",
-        type=str,
-        help="Path to the output folder containing files to process.",
+        "output", help="Path to the output folder to save results", type=str
     )
 
     parsed_args = parser.parse_args()
@@ -25,26 +24,10 @@ def parse_args():
     return parsed_args.input, parsed_args.output
 
 
-def preprocess_lines(lines):
-    pass
-
-
-def split_into_words(preprocessed_lines):
-    pass
-
-
-def count_words(words):
-    pass
-
-
-def write_word_counts(output_folder, word_counts):
-    pass
-
-
 def main():
     input_folder, output_folder = parse_args()
     lines = read_all_lines(input_folder)
     preprocessed_lines = preprocess_lines(lines)
-    words = split_into_words(preprocessed_lines)
+    words = split_lines_into_words(preprocessed_lines)
     word_counts = count_words(words)
     write_word_counts(output_folder, word_counts)
